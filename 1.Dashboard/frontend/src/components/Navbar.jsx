3cc48f8f668d56e783a45e32f7758df43fdbb0ed
sebin36 <94543955+sebin36@users.noreply.github.com>
2024-11-04 10:49:59 +0530

    Update Navbar.jsx

--- 1.Dashboard/frontend/src/components/Navbar.jsx
+++ 1.Dashboard/frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, NavLink, useLocation} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import {useLogout} from "../hooks/useLogout";
 import {useAuthContext} from "../hooks/useAuthContext";
 import {toast} from "react-toastify";
